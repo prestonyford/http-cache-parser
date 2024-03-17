f67329d0d647dc1e57f4fab9dd3fe093a9e6f9d9
Preston Ford <preston.y.ford@gmail.com>
2024-03-17 01:10:53 -0600

    javascript init

---
+++ public/httpParser.js
@@ -0,0 +1,43 @@
+
+// File signatures
+document.getElementById('preset').addEventListener("input", (event) => {
+    let preset = event.target.value;
+    console.log("Preset changed to " + preset);
+    changeSigs(preset);
+});
+
+const fileTypeToSignatures = {
+    "pdf": ["%PDF-"],
+    "png": ["\x89PNG\x0D\x0A\x1A\x0A"],
+    "jpg": ["\xFF\xD8\xFF\xE0", "\xFF\xD8\xFF\xE1", "\xFF\xD8\xFF\xE8"],
+    "gif": ["GIF87a", "GIF89a"],
+    "zip": ["PK\x03\x04"],
+    "exe": ["MZ"],
+    "mp3": ["ID3", "\xFF\xFB", "\xFF\xF3"],
+    "ogg": ["OggS"],
+};
+
+function changeSigs(preset) {
+    let sigs = document.getElementById('sigs');
+    if (preset in fileTypeToSignatures) {
+        sigs.value = fileTypeToSignatures[preset].map((type) => convertNonPrintableToHex(type)).join("\n");
+    }
+}
+
+function convertNonPrintableToHex(inputString) {
+    let result = '';
+    
+    for (let i = 0; i < inputString.length; i++) {
+      const charCode = inputString.charCodeAt(i);
+      
+      // Check if the character is non-printable
+      if (charCode < 32 || charCode > 126) {
+        // Convert non-printable character to its hexadecimal representation
+        result += '\\x' + charCode.toString(16).toUpperCase().padStart(2, '0');
+      } else {
+        result += inputString[i];
+      }
+    }
+    
+    return result;
+}
